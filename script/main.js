2dbb8de9e894ef2397f970a89199bd96f6b18e4c
DmitriyIvanovIT <strela199720@gmail.com>
2020-07-10 23:05:23 +0300

    День 4

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
+let counter = dataBase.length;
 
 // Переменные
 const modalAdd = document.querySelector('.modal__add'),
@@ -13,7 +14,9 @@ const modalAdd = document.querySelector('.modal__add'),
     modalFileInput = document.querySelector('.modal__file-input'),
     modalFileBtn = document.querySelector('.modal__file-btn'),
     modalImageAdd = document.querySelector('.modal__image-add'),
-    modalContent = modalItem.querySelector('.modal__content');
+    modalContent = modalItem.querySelector('.modal__content'),
+    searchInput = document.querySelector('.search__input'),
+    menuContainer = document.querySelector('.menu__container');
 
 const textFileBtn = modalFileBtn.textContent,
     srcImageAdd = modalImageAdd.src;
@@ -61,33 +64,32 @@ const closeModal = event => {
     };
 
 // Рендер
-const readerCard = () => {
-    catalog.textContent = '';
+const renderCard = (DB = dataBase) => {
+        catalog.textContent = '';
 
-    dataBase.forEach((item, i) => {
-        catalog.insertAdjacentHTML('beforeend',
-            `<li class="card" data-id="${i}">
+        DB.forEach(item => {
+            catalog.insertAdjacentHTML('beforeend',
+                `<li class="card" data-id="${item.id}">
                 <img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
                 <div class="card__description">
                     <h3 class="card__header">${item.nameItem}</h3>
                     <div class="card__price">${item.costItem} ₽</div>
                 </div>
             </li>`
-        );
-    });
-},
-renderModal = id => {
-    modalContent.textContent = '';
-    const e= +id;
+            );
+        });
+    },
+    renderModal = dataId => {
+        modalContent.textContent = '';
 
-    dataBase.forEach((item, i)=> {
-        if (i === e) {
-            modalContent.insertAdjacentHTML('beforeend',
+        const item = dataBase.find(item => item.id === +dataId);
+
+        modalContent.insertAdjacentHTML('beforeend',
             `
                 <div><img class="modal__image modal__image-item" src="data:image/jpeg;base64,${item.image}" alt="test"></div>
                 <div class="modal__description">
                     <h3 class="modal__header-item">${item.nameItem}</h3>
-                    <p>Состояние: <span class="modal__status-item">${item.status}</span></p>
+                    <p>Состояние: <span class="modal__status-item">${item.status === 'new' ? 'Новый' : 'Б/у'}</span></p>
                     <p>Описание:
                         <span class="modal__description-item">${item.descriptionItem}</span>
                     </p>
@@ -95,10 +97,19 @@ renderModal = id => {
                     <button class="btn">Купить</button>
                 </div>  
             `);
-        }
-    })
-}; 
 
+
+    };
+
+// Поиск
+searchInput.addEventListener('input', () => {
+    let valueSearch = searchInput.value.trim().toLowerCase();
+
+    if (valueSearch.length > 2) {
+        const result = dataBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch));
+        renderCard(result);
+    };
+})
 
 // Обработка событий
 
@@ -117,13 +128,16 @@ modalSubmit.addEventListener('submit', event => {
     for (const elem of elementsModalSubmit) {
         itemObj[elem.name] = elem.value;
     };
+    itemObj.id = counter++;
     itemObj.image = infoPhoto.base64;
     dataBase.push(itemObj);
     modalSubmit.reset();
     saveDB();
-    
-    closeModal({target: modalAdd});
-    readerCard();
+
+    closeModal({
+        target: modalAdd
+    });
+    renderCard();
 });
 
 modalFileInput.addEventListener('change', event => {
@@ -150,4 +164,13 @@ modalFileInput.addEventListener('change', event => {
 
 });
 
-readerCard(); 
+menuContainer.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.tagName === 'A') {
+        const result = dataBase.filter(item => item.category === target.dataset.category);
+        renderCard(result);
+    }
+})
+
+renderCard();
